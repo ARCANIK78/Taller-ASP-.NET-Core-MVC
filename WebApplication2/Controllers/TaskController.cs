602e63bf9d3c722917784cd0d1861a7e367d638e
ARCANIT78 <loisgordisrour@gmail.com>
2025-02-11 03:03:53 -0400

    Agregamos clases: taskcontroler taskmodel

---
+++ WebApplication2/Controllers/TaskController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Task = WebApplication2.Models.Task;
+namespace WebApplication2.Controllers
+{
+    public class TaskController : Controller
+    {
+        private static List<Task> tasks = new List<Task>();
+        private static int nextId = 1;
+
+        public IActionResult Index()
+        {
+            return View(tasks);
+        }
+        public IActionResult Details(int id)
+        {
+            var task = tasks.FirstOrDefault(x => x.Id == id);
+            if (task == null) {
+                return NotFound();
+            }
+            return View(task);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                task.Id = nextId++;
+                tasks.Add(task);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tasks);
+        }
+    }
+    
+}
---
+++ WebApplication2/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public bool isCompleted { get; set; }
+    }
+}
